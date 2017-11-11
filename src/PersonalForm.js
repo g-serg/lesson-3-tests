b34d23983cb880d643cee7df5e0ae869e8b752f8
g-serg <mr-sergey2004@yandex.ru>
2017-11-12 01:31:21 +0300

    person form tests done

--- src/PersonalForm.js
+++ src/PersonalForm.js
@@ -1,12 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Title from './Title';
 import './PersonalForm.css';
 
 export class PersonalForm extends Component {
+  handleChangeForm = event => {
+    const target = event.target;
+    const { onChangeForm } = this.props;
+    onChangeForm(target.name, target.value);
+  };
+
   render() {
     return (
       <div>
         <Title>Персональная информация</Title>
+        <div className="personal-form">
+          <input name="firstName" onChange={this.handleChangeForm} />
+          <input name="lastName" onChange={this.handleChangeForm} />
+          <input name="email" onChange={this.handleChangeForm} />
+        </div>
       </div>
     );
   }
