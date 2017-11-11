f424ea4cfdac5ee8b8ae3c89b1bde379b0140558
g-serg <mr-sergey2004@yandex.ru>
2017-11-12 02:11:00 +0300

    step test done

--- src/Step.js
+++ src/Step.js
@@ -1,9 +1,28 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import './Step.css';
 
 class Step extends PureComponent {
+  handleClick = () => {
+    const { isSelected, isClickable, number, onClick } = this.props;
+    isClickable && onClick(number);
+  };
+
   render() {
-    return <div />;
+    const { isSelected, isClickable, number, children } = this.props;
+
+    return (
+      <div
+        className={
+          'step' +
+          (isSelected ? ' step-selected' : '') +
+          (isClickable ? ' step-clickable' : '')
+        }
+        onClick={this.handleClick}
+      >
+        <div className="step__number">{number}</div>
+        <div className="step__title">{children}</div>
+      </div>
+    );
   }
 }
 
