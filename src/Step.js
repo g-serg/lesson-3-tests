07212df89e0594d082498ebcaaca1a0431d29df5
Sergey Gorelov <s.gorelov@norsi-trans.ru>
2017-11-14 11:10:55 +0300

    Revert "step test done"
    This reverts commit f424ea4cfdac5ee8b8ae3c89b1bde379b0140558.

--- src/Step.js
+++ src/Step.js
@@ -1,28 +1,9 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import './Step.css';
 
 class Step extends PureComponent {
-  handleClick = () => {
-    const { isSelected, isClickable, number, onClick } = this.props;
-    isClickable && onClick(number);
-  };
-
   render() {
-    const { isSelected, isClickable, number, children } = this.props;
-
-    return (
-      <div
-        className={
-          'step' +
-          (isSelected ? ' step-selected' : '') +
-          (isClickable ? ' step-clickable' : '')
-        }
-        onClick={this.handleClick}
-      >
-        <div className="step__number">{number}</div>
-        <div className="step__title">{children}</div>
-      </div>
-    );
+    return <div />;
   }
 }
 
